ea6fef45dc23a0c3536b3c40692497da126d1400
Daniel Zamorano <dzamorano@tacitknowledge.com>
2019-09-17 21:51:46 -0500

    New style, splitting global variables, removing particles from home.

--- src/home/home.js
+++ src/home/home.js
@@ -1,15 +1,22 @@
 import React from "react";
-import ParticlesComponent from "../components/particles-component";
 
 import "./home.scss";
 
 const Home = () => (
   <div className="home-page">
-    <ParticlesComponent id={"particles-home"} />
-    <div className="row home-page__header">
-      <div className="six column">
-        <h1 className="home-page__title"> Daniel Zamorano </h1>
-        <h4 className="home-page__subtitle"> Work in progress </h4>
+    <div className="home-page__content">
+      <div className="row home-page__header">
+        <div className="six column">
+          <h1 className="home-page__title"> Daniel Zamorano </h1>
+          <h4 className="home-page__subtitle"> Software Engineer </h4>
+        </div>
+      </div>
+      <div className="row home-page__intro">
+        <p className="six column">
+          I code solutions and lead teams. I can work remotelly and I do my best
+          in every project. I learn every day. I like to share knowledge and
+          teach.
+        </p>
       </div>
     </div>
   </div>
