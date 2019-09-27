b99a164e21c26ffe248fd391870aab0d6e4cb41a
Daniel Zamorano <dzamorano@tacitknowledge.com>
2019-09-27 14:51:41 -0500

    Make local navigation to work.

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, createRef } from "react";
+import React, {useEffect, createRef} from "react";
 import ConsoleTyper from "../../utils/console-typer";
 
 import "./home.scss";
@@ -8,15 +8,9 @@ const Home = () => {
   const consoleTyper = createRef();
 
   useEffect(() => {
-    consoleTyper.current = new ConsoleTyper({
-      paragraphElement: typerParagraph.current,
-      loop: false,
-      typingSpeedMs: 40,
-      stopCursorAfterBlinks: 200
-    });
-
+    consoleTyper.current = new ConsoleTyper({paragraphElement: typerParagraph.current, loop: false, typingSpeedMs: 40, stopCursorAfterBlinks: 200});
     setTimeout(() => consoleTyper.current.startTyping(), 1000);
-  });
+  }, []);
 
   return (
     <div className="home-page">
@@ -24,31 +18,40 @@ const Home = () => {
         <div className="row">
           <div className="six column">
 
-            <section className="home-page__header">
-              <h1 className="home-page__title"> Daniel Zamorano </h1>
-              <h4 className="home-page__subtitle"> Software Engineer </h4>
-            </section>
+            <section className="home-page__about" id="about">
+              <div className="home-page__header">
+                <h1 className="home-page__title">
+                  Daniel Zamorano
+                </h1>
+                <h4 className="home-page__subtitle">
+                  Software Engineer
+                </h4>
+              </div>
 
-            <section className="home-page__intro">
-              <p className="home-page__intro-text" ref={typerParagraph}>
-                I code software solutions for the web and lead teams. I work on-site or
-                remotely. I like to learn, experiment, share and teach.
-              </p>
+              <div className="home-page__intro">
+                <p className="home-page__intro-text" ref={typerParagraph}>
+                  I code software solutions for the web and lead teams. I work on-site or
+                  remotely. I like to learn, experiment, share and teach.
+                </p>
+              </div>
             </section>
-
           </div>
         </div>
 
         <div className="row">
           <div className="six column">
-            <section className="home-page__profesional-experience">
-              <h3> Professional Experience </h3>
+            <section className="home-page__profesional-experience" id="experience">
+              <h3>
+                Professional Experience
+              </h3>
               <p>
-                More than 10 years of experience building software solutions,
-                working with international teams with modern technologies,
-                from different sides: backend, frontend and design.
+                More than 10 years of experience building software solutions, working with
+                international teams and modern technologies, from different sides: backend,
+                frontend and design.
               </p>
-              <button className="btn"> See More </button>
+              <button className="btn">
+                See More
+              </button>
             </section>
           </div>
         </div>
