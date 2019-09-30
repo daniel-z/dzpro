69ed75023d668d7d9eaa5dfc84ec78f483b2bb82
Daniel Zamorano <dzamorano@tacitknowledge.com>
2019-09-30 11:56:38 -0500

    Adding projects for job experience and styles.

--- src/components/job-experience/job-experience.js
+++ src/components/job-experience/job-experience.js
@@ -2,23 +2,38 @@ import React from "react";
 import JobExperienceData from "../../data/job-experience";
 import "./job-experience.scss";
 
+const Projects = ({projects}) => {
+  return (projects.map((project) => (
+    <div>
+      <span className="job__project-name">{project.name}</span>
+      <p>{project.description}</p>
+    </div>
+  )));
+};
+
 const JobExperienceElement = ({
   company,
   position,
   dates,
   description,
-  keywords
+  keywords,
+  projects
 }) => {
   return (
     <div className="job">
       <h3 className="job__title">{position}</h3>
       <span className="job__company">
-        {company} / {dates}
+        {company}
+        / {dates}
       </span>
       <p className="job__description">{description}</p>
+
+      {projects && projects.length > 0 && <Projects projects={projects}/>}
+
       <p className="job__keywords">
         {keywords.map((word, idx) => (
-          <span key={`keyword-${idx}`}>{word + ", "} </span>
+          <span key={`keyword-${idx}`}>{word + ", "}
+          </span>
         ))}
       </p>
     </div>
@@ -26,12 +41,9 @@ const JobExperienceElement = ({
 };
 
 const JobExperience = () => {
-  console.log(JobExperienceData);
   return (
     <section className="experience">
-      {JobExperienceData.map((ExperienceData, idx) => (
-        <JobExperienceElement key={`job-${idx}`} {...ExperienceData} />
-      ))}
+      {JobExperienceData.map((ExperienceData, idx) => (<JobExperienceElement key={`job-${idx}`} {...ExperienceData}/>))}
     </section>
   );
 };
--- src/data/job-experience.js
+++ src/data/job-experience.js
@@ -3,8 +3,9 @@ const JobExperienceData = [
     company: "Tacit Knowledge (Based in San Francisco, CA)",
     position: "Frontend Engineer",
     dates: "March 17 - Today",
-    description:
-      "Building high performance e-commerce sites for big brands like: fitflop, wilko, solstice, using modern frameworks and technologies, working on SEO, accesibility, performance. Leading projects, recruiting and training.",
+    description: "Building high performance e-commerce sites for big brands like: fitflop, wilko, " +
+        "solstice, using modern frameworks and technologies, working on SEO, accesibility" +
+        ", performance. Leading projects, recruiting and training.",
     keywords: [
       "ReactJs",
       "Angular",
@@ -16,13 +17,15 @@ const JobExperienceData = [
       "Unit Testing",
       "Design Systems"
     ]
-  },
-  {
+  }, {
     company: "GoOut (Based in Guadalajara)",
     position: "CTO",
     dates: "June 2016 - March 2017",
-    description:
-      "Building a unique experiences marketplace. Hiring tech team, managing, organizing, developing, designing, taking development and business decisions. In charge of all Technology and team behind the platform. Leading designers and developers in the project. Using Python, Django, Angularjs, PostgreSQL, Heroku, Amazon Web Services, NodeJs, Google Analytics.",
+    description: "Building a unique experiences marketplace. Hiring tech team, managing, organizin" +
+        "g, developing, designing, taking development and business decisions. In charge o" +
+        "f all Technology and team behind the platform. Leading designers and developers " +
+        "in the project. Using Python, Django, Angularjs, PostgreSQL, Heroku, Amazon Web " +
+        "Services, NodeJs, Google Analytics.",
     keywords: [
       "Python",
       "Django",
@@ -37,40 +40,56 @@ const JobExperienceData = [
       "ES6",
       "ES7"
     ]
-  },
-  {
+  }, {
     company: "BairesDev (Based in Argentina)",
     position: "Senior Frontend Developer",
     dates: "May 2015 - June 2016",
-    description: "",
+    description: "Working 100% remotelly for companies and projects in the US, sometimes leading a" +
+        "nd other times being part of an extended team",
     projects: [
       {
         name: "Vans Customs ​(California)",
-        description:
-          "Developing the new interface and fixing frontend issues in the new Vans 3D interactive shoe configurator. Working with a distributed team in the US. Vans Customs"
-      },
-      {
+        description: "Developing the new interface and fixing frontend issues in the new Vans 3D inter" +
+            "active shoe configurator. Working with a distributed team in the US. Vans Custom" +
+            "s"
+      }, {
         name: "Basepath ​(Boston)",
-        description:
-          "Working in the development and maintenance of basepath medical reporting tool. Built using backbone and marionette. I’m working in the full app improving the code, architecture and user experience. I implemented new standards that improved the development and maintenance of the code."
+        description: "Working in the development and maintenance of basepath medical reporting tool. B" +
+            "uilt using backbone and marionette. I’m working in the full app improving the co" +
+            "de, architecture and user experience. I implemented new standards that improved " +
+            "the development and maintenance of the code."
       }
     ],
-    keywords: ["Backbone", "Marionette", "CouchDB", "CSS3", "ES5", "TreeJS"]
-  },
-  {
+    keywords: [
+      "Backbone",
+      "Marionette",
+      "CouchDB",
+      "CSS3",
+      "ES5",
+      "TreeJS"
+    ]
+  }, {
     company: "iTexico (Based in Guadalajara)",
     position: "Senior Frontend Developer",
     dates: "Agosto 2014  - Julio 2015",
-    description:
-      "Working in the development and maintenance of the Carbon Black web application for Bit9 company in Texas. I improved the development environment by implementing Vagrant, encouraging JS, CSS, Backbone and templating best practices.",
-    keywords: ["Backbone", "Marionette", "CouchDB", "CSS3", "ES5", "TreeJS"]
-  },
-  {
+    description: "Working in the development and maintenance of the Carbon Black web application f" +
+        "or Bit9 company in Texas. I improved the development environment by implementing" +
+        " Vagrant, encouraging JS, CSS, Backbone and templating best practices.",
+    keywords: [
+      "Backbone",
+      "Marionette",
+      "CouchDB",
+      "CSS3",
+      "ES5",
+      "TreeJS"
+    ]
+  }, {
     company: "Wiser Together ​(Based in Washington)",
     position: "Lead Frontend Developer",
     dates: "April 2013 – March 2014",
-    description:
-      "Working on a medical decisions tool, using modern client-side technologies, JavaScript unit testing and graph frameworks including d3, working remotely with teams from US, Spain and Mexico. I ended up leading a successful team.",
+    description: "Working on a medical decisions tool, using modern client-side technologies, Java" +
+        "Script unit testing and graph frameworks including d3, working remotely with tea" +
+        "ms from US, Spain and Mexico. I ended up leading a successful team.",
     keywords: [
       "d3",
       "backbone",
@@ -80,21 +99,31 @@ const JobExperienceData = [
       "sinon",
       "karma"
     ]
-  },
-  {
+  }, {
     company: "Tacit Knowledge ​(Based in San Francisco)",
     position: "Web Developer",
-    description:
-      "Working on big e-commerce web-sites, I worked in the shopping car process for HEB, and the responsive version of the GhdHair England site with support for Ipad and mobile phones, working with teams from US, England and Mexico.",
+    description: "Working on big e-commerce web-sites, I worked in the shopping car process for HE" +
+        "B, and the responsive version of the GhdHair England site with support for Ipad " +
+        "and mobile phones, working with teams from US, England and Mexico.",
     dates: "June 2011 – April 2013",
-    keywords: ["Gulp", "Grunt", "JQuery", "AEM", "Javascript", "HTML", "CSS"]
-  },
-  {
+    keywords: [
+      "Gulp",
+      "Grunt",
+      "JQuery",
+      "AEM",
+      "Javascript",
+      "HTML",
+      "CSS"
+    ]
+  }, {
     company: "OTRS AG ​(Based in Germany)",
     position: "Software Engineer",
     dates: "February 2010 – June 2011",
-    description:
-      "Frontend/Backend development for a German company, Trained in Germany, working with linux, mac and osx equipments, doing OTRS software maintenance, responsible of survey project and the data encryption tool, improving markedly the speed and other characteristics of it, development for German customers, supporting and interacting with customers and community, forums, email lists etc.",
+    description: "Frontend/Backend development for a German company, Trained in Germany, working w" +
+        "ith linux, mac and osx equipments, doing OTRS software maintenance, responsible " +
+        "of survey project and the data encryption tool, improving markedly the speed and" +
+        " other characteristics of it, development for German customers, supporting and i" +
+        "nteracting with customers and community, forums, email lists etc.",
     keywords: [
       "Gulp",
       "Grunt",
@@ -107,13 +136,14 @@ const JobExperienceData = [
       "Backend",
       "Frontend"
     ]
-  },
-  {
+  }, {
     company: "Dux Diligens ​(Based in Mexico D.F.)",
     position: "Software Development & IT Consultant",
     dates: "January 2008 – February 2010",
-    description:
-      "Development of Open Source Software, design of IT solutions based on ITIL methodology, giving ITIL training courses, implementation of software tools for government companies and private companies like Palacio de Hierro, Writing technical and user Manuals, Product Demos.",
+    description: "Development of Open Source Software, design of IT solutions based on ITIL method" +
+        "ology, giving ITIL training courses, implementation of software tools for govern" +
+        "ment companies and private companies like Palacio de Hierro, Writing technical a" +
+        "nd user Manuals, Product Demos.",
     keywords: [
       "CSS",
       "Perl",
