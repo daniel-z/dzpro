8ba8c49ffba557b85325522396a1a37a38689d20
Daniel Zamorano <dzamorano@tacitknowledge.com>
2019-09-30 11:26:19 -0500

    Adding smooth scroll for header.

---
+++ src/pages/home/header/header.js
@@ -0,0 +1,44 @@
+import React, {useEffect} from 'react';
+import './header.scss';
+
+function scrollTo(ev) {
+  ev.preventDefault();
+  const target = ev.target;
+  if (target && target.attributes && target.attributes.href && target.attributes.href.nodeValue) {
+    const toElementId = target
+      .attributes
+      .href
+      .nodeValue
+      .slice(1);
+    const toElement = document.getElementById(toElementId);
+    if (toElement) {
+      toElement.scrollIntoView({block: 'end', behavior: 'smooth'});
+    }
+  }
+}
+
+export default() => {
+  useEffect(() => {
+    const links = new Array(...document.getElementsByClassName("local-nav__link"));
+
+    links.forEach(link => {
+      link.addEventListener('click', scrollTo);
+    });
+  }, []);
+
+  return (
+    <header className="app-header">
+      <nav className="local-nav">
+        <a className="local-nav__link" href={'#about'}>
+          <b>01.</b>
+          About</a>
+        <a className="local-nav__link" href={'#experience'}>
+          <b>02.</b>
+          Experience</a>
+        <a className="local-nav__link" href={'#coding-garage'}>
+          <b>03.</b>
+          Coding Garage</a>
+      </nav>
+    </header>
+  )
+};
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -15,45 +15,37 @@ const Home = () => {
   return (
     <div className="home-page">
       <div className="home-page__content">
-        <div className="row">
-          <div className="six column">
-            <section className="home-page__about" id="about">
-              <div className="home-page__header">
-                <h1 className="home-page__title">
-                  Daniel Zamorano
-                </h1>
-                <h4 className="home-page__subtitle">
-                  Software Engineer
-                </h4>
-              </div>
-
-              <div className="home-page__intro">
-                <p className="home-page__intro-text" ref={typerParagraph}>
-                  I code software solutions for the web and lead teams. I work on-site or
-                  remotely. I like to learn, experiment, share and teach.
-                </p>
-              </div>
-            </section>
+        <section className="home-page__about" id="about">
+          <div className="home-page__header">
+            <h1 className="home-page__title">
+              Daniel Zamorano
+            </h1>
+            <h4 className="home-page__subtitle">
+              Software Engineer
+            </h4>
           </div>
-        </div>
 
-        <div className="row">
-          <div className="six column">
-            <section className="home-page__profesional-experience" id="experience">
-              <h3>
-                Professional Experience
-              </h3>
-              <p>
-                More than 10 years of experience building software solutions, working with
-                international teams and modern technologies, from different sides: backend,
-                frontend and design.
-              </p>
-              <button className="btn">
-                See More
-              </button>
-            </section>
+          <div className="home-page__intro">
+            <p className="home-page__intro-text" ref={typerParagraph}>
+              I code software solutions for the web and lead teams. I work on-site or
+              remotely. I like to learn, experiment, share and teach.
+            </p>
           </div>
-        </div>
+        </section>
+
+        <section className="home-page__profesional-experience" id="experience">
+          <h3>
+            Professional Experience
+          </h3>
+          <p>
+            More than 10 years of experience building software solutions, working with
+            international teams and modern technologies, from different sides: backend,
+            frontend and design.
+          </p>
+          <a className="btn" href="/cv">
+            See More
+          </a>
+        </section>
       </div>
     </div>
   );
