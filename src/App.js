3b1cc3dc205cd7ab908692faa45d08d9def4e296
Daniel Zamorano <dzamorano@tacitknowledge.com>
2019-10-02 16:52:17 -0500

    Replacing Router by HashRouter to skip url problems with nginx.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as HashRouter, Route} from "react-router-dom";
 import Home from "./pages/home/home";
 import WIP from "./pages/wip/wip";
 import MainLayout from "./layouts/main-layout";
@@ -11,14 +11,14 @@ import "./App.scss";
 class App extends Component {
   render() {
     return (
-      <Router>
+      <HashRouter basename="/#">
         <MainLayout className="app">
           <Route exact path={ROUTES.HOME} component={Home}/>
           <Route exact path={ROUTES.EXPERIENCE} component={Experience}/>
           <Route exact path={ROUTES.ABOUT} component={WIP}/>
           <Route exact path={ROUTES.CODE} component={WIP}/>
         </MainLayout>
-      </Router>
+      </HashRouter>
     );
   }
 }
